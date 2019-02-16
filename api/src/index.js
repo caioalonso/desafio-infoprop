61d4a86ff61302fca84c9a1e22a0f741335f1925
Caio Alonso <caio@caioalonso.com>
2019-02-16 17:24:04 -0200

    Adds CORS to API

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,11 @@
 const Express = require('express');
+const cors = require('cors');
 const v1 = require('./routes/v1');
 require('./db');
 
 const app = Express();
 
+app.use(cors())
 app.use('/v1', v1);
 
 app.use((req, res, next) => {
